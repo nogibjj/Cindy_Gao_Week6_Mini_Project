7af374c6f1f32a63af8f9ce84c1326d3b20350ae
Jiaxin-Cindy <jiaxin.gao1997@gmail.com>
2024-10-11 21:51:10 -0400

    change env

--- mylib/query.py
+++ mylib/query.py
@@ -11,9 +11,9 @@ def query(dataset="data/murder_2015_final.csv"):
     payload = csv.reader(open(dataset, newline=""), delimiter=",")
     next(payload)
     load_dotenv()
-    server_h = os.getenv("sql_server_host")
-    access_token = os.getenv("databricks_api_key")
-    http_path = os.getenv("sql_http")
+    server_h = os.getenv("SQL_SERVER_HOST")
+    access_token = os.getenv("DATABRICKS_API_KEY")
+    http_path = os.getenv("SQL_HTTP")
     with sql.connect(
         server_hostname=server_h,
         http_path=http_path,
